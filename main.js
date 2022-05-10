cb59b732e0b430a35144f92d162a497676aefa97
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-10 13:08:41 -0400

    Added styling continued

--- main.js
+++ main.js
@@ -5,10 +5,30 @@ window.addEventListener('load', () => {
         let contents = $('#contents');
         let contentsParent = contents.parent();
         contents.remove();
+
         let img = new Image();
         img.src = chrome.runtime.getURL('/images/DeGods-5011-dead.png');
+        img.width = 500;
+        img.height = 500;
         contentsParent.prepend(img);
-        let title = $("<div>").html("DeadGod 5011");
-        contentsParent.prepend(title);
+
+        let title = $("<h1>").html("DeadGod 5011");
+        title.addClass('beautText');
+        contentsParent.append(title);
+
+        setTimeout(() => {
+            let left = $('.beautText').offset().left;
+            console.log(left);
+
+            $('.beautText').click(() => {
+                title.css({'left': left}).animate({left: '500px', right: ''}, 'slow');
+                img.animate({height: '100px'});
+                img.animate({right: '100px'});
+                console.log('animating title');
+            // title.animate({left: '', right: '200px'});
+            })
+        }, 2000);
+        
     }, 1000);
 })
+
