618fe16bcfb0c257e52b6a9e39da7f60e6b10463
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-11 13:52:15 -0400

    Added event that shifts img and title

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // JQuery
 // This function waits for the entire page to be loaded, then waits a second until the id contents is grabbed and removed from the page
-window.addEventListener('load', () => {
-    window.setTimeout(() => {
+$(window).on('load', () => {
+    setTimeout(() => {
         let contents = $('#contents');
         let contentsParent = contents.parent();
         contents.remove();
@@ -12,22 +12,16 @@ window.addEventListener('load', () => {
         img.height = 500;
         contentsParent.prepend(img);
 
-        let title = $("<h1>").html("DeadGod 5011");
+        let title = $("<h1>");
+        title.html("DeadGod 5011");
         title.addClass('beautText');
         contentsParent.append(title);
 
-        setTimeout(() => {
-            let left = $('.beautText').offset().left;
-            console.log(left);
-
-            $('.beautText').click(() => {
-                title.css({'left': left}).animate({left: '500px', right: ''}, 'slow');
-                img.animate({height: '100px'});
-                img.animate({right: '100px'});
-                console.log('animating title');
-            // title.animate({left: '', right: '200px'});
-            })
-        }, 2000);
+        // Clicking the title element will move both img and title 200px to the left
+        $('.beautText').click(() => {
+            $(img).css('position', 'relative').animate({left: '200px',}, 3000); // position must be set to relative otherwise left or right won't work
+            $(title).css('position', 'relative').animate({left: '200px'}, 'slow'); // ""    ""     "" 
+        })
         
     }, 1000);
 })
