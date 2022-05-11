236a9bff2a847a136eeb422f60f2dd3bbfa58b05
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-11 17:29:32 -0400

    Added ajax request to retrieve an array and randomly select one to display in the dom

--- main.js
+++ main.js
@@ -5,6 +5,32 @@ for (let image of images) {
     imgUrls.push(chrome.runtime.getURL(`/images/${image}.png`));
 }
 
+// Makes an AJAX GET request to the URL, and it responds with an array of objects that each represent 
+// data about an image. We fetch one randomly by its image ID, and populate it to the DOM
+let ajaxImgs = [];
+let randomImg = '';
+$.ajax({
+    method: 'GET',
+    url: 'https://picsum.photos/list',
+    dataType: 'json',
+    success: function(result) {
+        console.log('Saved result to ajaxImgs');
+        ajaxImgs = result;
+    },
+    error: function(err) {
+     console.log('There was an error');
+     console.log(err);
+    }
+}).done(() => {
+    let randomNum = getRandomArbitrary(0, ajaxImgs.length);
+    randomImg = ajaxImgs[randomNum];
+});
+
+// Gets random whole number between min and max
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 // This function waits for the entire page to be loaded, then waits a second until the id contents is grabbed and removed from the page
 $(window).on('load', () => {
     setTimeout(() => {
@@ -18,6 +44,12 @@ $(window).on('load', () => {
         img.height = 500;
         $(img).css('position', 'relative'); // position must be set to relative otherwise left or right won't work
         contentsParent.prepend(img);
+
+        let img1 = new Image();
+        img1.width = 500;
+        img1.height = 500;
+        img1.src = 'https://unsplash.it/1200/800?image=' + randomImg.id;
+        contentsParent.append(img1);
 
         let title = $("<h1>");
         title.html("DeadGod 5011");
@@ -33,7 +65,7 @@ $(window).on('load', () => {
         $('.beautText').click(() => {
             $(img).animate({left: '200px',}, 3000); 
             $(title).animate({left: '200px'}, 'slow');
-        })
+        });
         
 
         // Button that allows user to cycle through images and changes title to corresponding image 
@@ -45,7 +77,6 @@ $(window).on('load', () => {
             }
             img.src = imgUrls[index];
             title.html(`${images[index++]}`);
-        })
+        });
     }, 1000);
 })
-
